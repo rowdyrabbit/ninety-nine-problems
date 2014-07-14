b272f07ad6298d5fd4e1cebf8c90b996422df148
Denise Fernandez <dcbfernandez@gmail.com>
2014-07-14 11:31:47 -0400

    Problem 2 complete

--- src/main/scala/Problems.scala
+++ src/main/scala/Problems.scala
@@ -1,12 +1,15 @@
 
 object Problems {
 
-  //Problem 1
+  // Problem 1
   def last(list: List[Int]): Int = {
     list.foldLeft(List[Int]()){(accList, elem) => elem :: accList}.head
   }
 
-  
+  // Problem 2
+  def penultimate(list: List[Int]): Int = {
+    list.reverse.tail.head
+  }
 
 
 }
--- src/main/scala/test.sc
+++ src/main/scala/test.sc
@@ -3,3 +3,6 @@ import Problems._
 val nums = List(1, 2, 3, 4, 5)
 
 Problems.last(nums)
+
+
+Problems.penultimate(nums)
